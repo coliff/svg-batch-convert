c1400ec558d2e4f4d6a713e80ee1c268ff6d1046
coliff <christianoliff@pm.me>
2024-02-28 00:53:24 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SVG Batch Image Generator
+
+A script to generate images from SVGs.
+
+## Setup
+
+1. It requires [imageMagick](https://imagemagick.org/script/download.php) to be installed.
+_Note: When installing be sure to check the 'Legacy Convert' feature_
+2. Once ImageMagick is installed, restart VS Code if it is already open.
+3. Run `npm install` to install the required dependencies (Sharp is required to get the SVG size attributes).
+
+## Usage
+
+Run `npm run build` to output all icons as PNGs in the relevant folders. In the event of needing a different size you can edit the sizes on line 10 of `build.js`.
+
+## Notes
+
+- Running the build script again does not delete any files in the 'dist' folder prior to running.
---
+++ build.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
+const sharp = require("sharp");
+
+const svgDir = "./src"; // directory containing SVG icons
+const outputDir = "./dist"; // directory to save PNG icons
+
+// sizes you want to convert to
+const sizes = [220, 790];
+
+fs.readdir(svgDir, (err, files) => {
+  if (err) {
+    console.error(`Error reading directory: ${err}`);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (path.extname(file) === ".svg") {
+      const svgFile = path.join(svgDir, file);
+      const baseName = path.basename(file, ".svg");
+
+      sharp(svgFile)
+        .metadata()
+        .then((metadata) => {
+          const baselineSize = metadata.width; // baseline size
+
+          sizes.forEach((size) => {
+
+            const sizeDir = path.join(outputDir, `${size}`);
+            if (!fs.existsSync(sizeDir)) {
+              fs.mkdirSync(sizeDir);
+            }
+
+            const outputFile = path.join(sizeDir, `${baseName}.png`);
+            const cmd = `convert -background none -density 5000 -resize ${size}x${size} ${svgFile} ${outputFile}`;
+
+            exec(cmd, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`Error executing command: ${error}`);
+                return;
+              }
+              console.log(`Generated ${outputFile}`);
+            });
+          });
+        })
+        .catch((err) => {
+          console.error(`Error reading SVG metadata: ${err}`);
+        });
+    }
+  });
+});
